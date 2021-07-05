70540f7563649f145f611a765e1f51a9b579b8dc
Miguel Nava <guiguelangel@gmail.com>
2021-07-05 12:59:30 -0500

    Add practices to portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -16,11 +16,23 @@ export class PortfolioComponent implements OnInit {
 
     this.itemP = [
       {
-        name: 'energy solutions',
-        description: 'portfolio.description-ns',
-        urlImg: 'https://github.com/Navaxa/Miguel-Nava/blob/master/img/EnergySolution.png?raw=true',
-        urlPortfolio: 'https://work-click.000webhostapp.com/index.php'
-      },
+        name: 'cotizador criptomonedas',
+        description: 'React Js',
+        urlImg: './assets/statics/img/cotizador_crypto.PNG',
+        urlPortfolio: 'https://navaxa.github.io/criptomonedas/'
+      },    
+      {
+        name: 'administrador de pacientes',
+        description: 'React Js',
+        urlImg: './assets/statics/img/administrador_de_pacientes.jpg',
+        urlPortfolio: 'https://navaxa.github.io/administrar-citas-react-app/'
+      },    
+      {
+        name: 'crowfunding stellar react app',
+        description: 'React Js',
+        urlImg: './assets/statics/img/crowfunding.jpg',
+        urlPortfolio: 'https://navaxa.github.io/crowfunding-stellar-react-app/'
+      },    
       {
         name: 'YoCompartoMexico',
         description: 'portfolio.description-ycm',
@@ -28,10 +40,22 @@ export class PortfolioComponent implements OnInit {
         urlPortfolio: 'https://www.behance.net/gallery/108999357/YoCompartoMexico'
       },
       {
+        name: 'portfolio.nameMN',
+        description: 'JavaScript, HTML, CSS, Chatbot',
+        urlImg: './assets/statics/img/mn.jpg',
+        urlPortfolio: 'https://navaxa.github.io/Miguel-Nava/'
+      },
+      {
         name: 'batatabit',
-        description: '',
+        description: 'HTML, CSS',
         urlImg: 'https://github.com/Navaxa/Miguel-Nava/blob/master/img/portafolio.PNG?raw=true',
         urlPortfolio: 'https://www.behance.net/gallery/110279205/Batatabit'
+      },
+      {
+        name: 'energy solutions',
+        description: 'portfolio.description-ns',
+        urlImg: 'https://github.com/Navaxa/Miguel-Nava/blob/master/img/EnergySolution.png?raw=true',
+        urlPortfolio: 'https://work-click.000webhostapp.com/index.php'
       }
     ]
 
